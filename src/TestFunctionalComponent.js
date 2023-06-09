55d6452c5bd6a877c7485cdabf3a8de6dc39dedf
Manasvi0910 <97155655+Manasvi0910@users.noreply.github.com>
2023-06-09 15:16:15 +0530

    first commit

---
+++ src/TestFunctionalComponent.js
@@ -0,0 +1,8 @@
+const TestFunctionalComponent = () => {
+    return(
+        <div>
+            <h1>This is a functional Component</h1>
+        </div>
+    )
+}
+export default TestFunctionalComponent;
